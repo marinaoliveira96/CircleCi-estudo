795d3350d9e6e802d94ab8cef4623a40a5b38678
Marina Oliveira <marina09oliveira@gmail.com>
2020-09-13 19:55:33 -0300

    Base do projeto para o circle ci

---
+++ src/model/product.js
@@ -0,0 +1,7 @@
+export default class Product {
+  constructor(description, buyPrice, sellPrice, stock){
+    this.description = description;
+    this.buyPrice = sellPrice;
+    this.stock = stock;
+  }
+}
